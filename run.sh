4b55a624d943ad558b90c4a5b18c66fcb3a02085
hitode909 <hitode909@gmail.com>
2017-09-22 19:37:00 +0900

    Revert "500"
    This reverts commit c6996ca9711ffe8830802fa41fadd5d22e22d279.

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ set -x
 
 mkdir -p Downloads/
 rm Downloads/*
-docker run -i --rm -u 500:500 --name puppeteer-chrome --cap-add=SYS_ADMIN --shm-size=1gb --volume `pwd`:/home/pptruser puppeteer-chrome-linux node capture.js $*
+docker run -i --rm --name puppeteer-chrome --cap-add=SYS_ADMIN --shm-size=1gb --volume `pwd`:/home/pptruser puppeteer-chrome-linux node capture.js $*
